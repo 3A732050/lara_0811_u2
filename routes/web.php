5f72dabf652c07549d96493adf266de02f688154
林駿綦 <r10101720307@gmail.com>
2020-10-20 18:26:23 +0800

    練習1： 設定 Route 跳轉頁面

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return 'welcome';
 });
+*/
+
+Route::get('/',function() {
+    return redirect('r1');
+});
+
+Route::get('r1', function() {
+    return redirect('r2');
+});
+
+Route::get('r2', function() {
+    return view('welcome');
+});
+
