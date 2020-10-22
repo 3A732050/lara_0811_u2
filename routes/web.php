359d6db688ea817610cc1c9c7cdd08de05b9ee9a
林駿綦 <r10101720307@gmail.com>
2020-10-22 17:44:56 +0800

    練習5：設定另一個 Route ，並以群組（group）包起來設定 prefix

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,14 @@ Route::get('hello/{name?}', function ($name='Everybody') {
     return 'Hello, ' . $name;
 })->name('hello.index');
 */
+
 Route::get('dashboard', function() {
     return 'dashboard';
+});
+
+Route::group(['prefix'=>'admin'], function() {
+    Route::get('dashboard', function() {
+        return 'admin dashboard';
+    });
 });
 
